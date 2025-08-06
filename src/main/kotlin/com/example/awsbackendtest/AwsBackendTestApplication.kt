6a859f5d41404725188c56d3e8a5f0919a88ac33
minseok <xhdldlsl@gmail.com>
2025-08-06 15:35:27 +0900

    init : aws backend test

---
+++ src/main/kotlin/com/example/awsbackendtest/AwsBackendTestApplication.kt
@@ -0,0 +1,11 @@
+package com.example.awsbackendtest
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AwsBackendTestApplication
+
+fun main(args: Array<String>) {
+    runApplication<AwsBackendTestApplication>(*args)
+}
